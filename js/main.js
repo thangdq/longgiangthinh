c02912cc40031affe0a5eae2532fd0bd8df2e6b1
Quang Thang <quangthang44@gmail.com>
2017-05-07 10:20:10 +0700

    fix(Search): Search mobile Search mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
   if($('.banner-container .owl-carousel').length){
     var options = {
       animateOut: 'fadeOut',
@@ -15,13 +15,13 @@ $(document).ready(function() {
     var owl_banner = $('.banner-container .owl-carousel').owlCarousel(options);
     owl_banner.trigger('refresh.owl.carousel');
   }
-  
+
   if($('.products-container .owl-carousel').length){
     var options = {
       items: 4,
       loop: true,
 	  autoplay: false,
-      
+
       responsive : {
           // breakpoint from 0 up
           0 : {
@@ -47,24 +47,37 @@ $(document).ready(function() {
     var owl_service = $('.products-container .owl-carousel').owlCarousel(options);
     owl_service.trigger('refresh.owl.carousel');
   }
-  
+
    // Open menu mobile
   $('#btn-open-menu-mobile').click(function() {
     $('.mobile-nav-popup-container').addClass('open');
   });
-  
+
   // Close menu mobile
   $('#btn-close-menu-mobile').click(function() {
     $('.mobile-nav-popup-container').removeClass('open');
   });
- 
+
+  // Open search form
+ $('#btn-search-mobile').click(function() {
+   $('.dropdown-menu.dropdown-form-search').addClass('active');
+ });
+
+ // Close search form
+ $(document).mouseup(function(e) {
+     var $container = $(".dropdown-menu.dropdown-form-search");
+     if (!$container.is(e.target) && $container.has(e.target).length === 0) {
+         $container.removeClass('active');
+     }
+ });
+
   $('.sidebar-container .sidebar-title').click(function() {
     var sidebar = $('.sidebar-container');
     if(sidebar.hasClass('open')){
       sidebar.removeClass('open')
     }else{
-      sidebar.addClass('open')      
+      sidebar.addClass('open')
     }
   });
-  
+
 });
